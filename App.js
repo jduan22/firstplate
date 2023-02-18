dc4578af827f3e3a5ff29a0dadbd4a69dbe34fc3
jduan22 <jenny.s.duan@gmail.com>
2023-02-18 10:14:21 -0800

    login screen in progress

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+
+import { LoginScreen } from "./app/screens";
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <LoginScreen />
+      </NavigationContainer>
+      {/* <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" /> */}
     </View>
   );
 }
---
+++ assets/Themes/colors.js
@@ -0,0 +1,13 @@
+export const colors = {
+  sage: '#D1E0BF',
+  gray: '#B3B3B3',
+  white: '#FFFFFF',
+  black: '#000000',
+  purple: '#858BDB',
+  red: '#db5442',
+  brown: '#af785f',
+  shadow: 'rgba(0, 0, 0, 0.25)',
+  darkerPurple: '#5257a1',
+  darkerSage: '#809c87',
+  darkerGray: '#767676',
+};
